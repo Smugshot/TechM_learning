e17984c9f4fcff56d551d5b35be66baf8c3a7cd7
Parth Dange <131579272+Smugshot@users.noreply.github.com>
2025-07-12 00:52:02 +0530

    Java: Basic

---
+++ java-core/Day1_Basics.java
@@ -0,0 +1,31 @@
+// public class Day1_Basics {
+//     public static void main(String[] args) {
+//         String name = "parth";
+//         int age = 10;
+//         System.out.println(name +',' + age);         
+//     } 
+// }
+
+
+/*   Methods  */
+// public class Day1_Basics {
+
+//     public static int add(int a , int b){
+//         return a*b;
+//     }
+
+//     public static void greet(){
+//         System.out.println("helloooooooooooooooooooooooooooooo!");
+//     }
+
+//     public static void Dispayinfo(String name ,  int age){
+//         System.out.println(name +','+ age);
+//     }
+
+//     public static void main(String[] args) {
+//         greet();
+//         int result = add(10 , 20);
+//         System.out.println(result);
+//         Dispayinfo( "Parth" , 10);
+//     }
+// }
