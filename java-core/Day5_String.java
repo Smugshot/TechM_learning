8709fdc271ae52dba91141c787e7345930a364bf
Parth Dange <131579272+Smugshot@users.noreply.github.com>
2025-08-21 12:43:26 +0530

    STRING practise

--- java-core/Day5_String.java
+++ java-core/Day5_String.java
@@ -4,7 +4,10 @@ public class Day5_String {
     
     public static void main(String[] args) {
         String name = "Elon Musk";
-    
+        String name2 = new String("Elon Musk");
+        String Name1 = "Elon Musk";
+
+        
         Scanner sc = new Scanner(System.in);
         String Name2 = sc.nextLine();
         // String Name2 = sc.next();   The diffrence is that next() take only one word as input and nextline() take complete line as input with space 
@@ -12,7 +15,13 @@ public class Day5_String {
         System.out.println(name);
         System.out.println(Name2);
         
+        System.out.println(Name1 == name);          // true (same pool)
+        System.out.println(Name1 == name2);         // false (different object)
+        System.out.println(Name1.equals(name2));    // true (content check)
 
+        
+        
+        
 
         // String methods:-
 
@@ -22,6 +31,10 @@ public class Day5_String {
         System.out.println(Name2.contains("arth"));
         System.out.println(Name2.toUpperCase());
         System.out.println(Name2.replace( "a", "xx"));
+
+        for(int i = 0; i < Name2.length() ; i++){
+            System.out.println(Name2.charAt(i));
+        }
 
     }
     
